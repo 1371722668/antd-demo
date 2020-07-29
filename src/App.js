19c250c111d6249b919d84568031ff9d50dce72f
sudongxue <1371722668@qq.com>
2020-07-29 15:15:45 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'antd'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Button type="primary">按钮</Button>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
